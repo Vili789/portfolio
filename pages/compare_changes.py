948f31ffc04f1d8777457a981fdb202df03d110a
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 13:45:42 +0100

    Update compare_changes.py

--- pages/compare_changes.py
+++ pages/compare_changes.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+st.set_page_config(page_title="Compare Changes Over Time")#, page_icon="📊")
+
+with st.sidebar.expander("📊 Instagram Analyzer"):
+    st.page_link("pages/SeeWhoDoesntFollowYou.py", label="Home Page")
+    st.page_link("pages/doesnt_follow.py", label="See Who Doesn't Follow You Back")
+    st.page_link("pages/compare_changes.py", label="Compare Changes Over Time")
+
+with st.sidebar.expander("🤖 Flashcard Automation Tool"):
+    st.page_link("pages/FlashcardAutomation.py", label="Home Page")
+    
+with st.sidebar.expander("🎰 Casino"):
+    st.page_link("pages/Casino.py", label="Home Page")
