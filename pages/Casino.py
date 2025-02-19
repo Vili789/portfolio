9a45c92f1f325000acc6e93a4d8949568fac922b
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 14:07:09 +0100

    Update Casino.py

--- pages/Casino.py
+++ pages/Casino.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Casino Game", page_icon="🎰")
 
+with st.sidebar.expander("Welcome!"):
+    st.page_link("app.py", label="Intorduction")
+    
 with st.sidebar.expander("📊 Instagram Analyzer"):
     st.page_link("pages/SeeWhoDoesntFollowYou.py", label="Home Page")
 
