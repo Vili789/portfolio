f7199238f32d514648d295ab3cf47ffa9e86632e
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 13:44:23 +0100

    Update SeeWhoDoesntFollowYou.py

--- pages/SeeWhoDoesntFollowYou.py
+++ pages/SeeWhoDoesntFollowYou.py
@@ -4,6 +4,8 @@ st.set_page_config(page_title="Instagram Analyzer", page_icon="📊")
 
 with st.sidebar.expander("📊 Instagram Analyzer"):
     st.page_link("pages/SeeWhoDoesntFollowYou.py", label="Home Page")
+    st.page_link("pages/doesnt_follow.py", label="See Who Doesn't Follow You Back")
+    st.page_link("pages/compare_changes.py", label="Compare Changes Over Time")
 
 with st.sidebar.expander("🤖 Flashcard Automation Tool"):
     st.page_link("pages/FlashcardAutomation.py", label="Home Page")
