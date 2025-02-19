8ad18d14cb33b3f3eb28748dd2ef3e3d5bd48635
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 14:07:25 +0100

    Update FlashcardAutomation.py

--- pages/FlashcardAutomation.py
+++ pages/FlashcardAutomation.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="Flashcard Automation Tool", page_icon="🤖")
 
+with st.sidebar.expander("Welcome!"):
+    st.page_link("app.py", label="Intorduction")
+    
 with st.sidebar.expander("📊 Instagram Analyzer"):
     st.page_link("pages/SeeWhoDoesntFollowYou.py", label="Home Page")
 
