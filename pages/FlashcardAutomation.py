4d99fc9b25b3d8cf567da6e054e51199bc32984f
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 13:38:08 +0100

    Update FlashcardAutomation.py

--- pages/FlashcardAutomation.py
+++ pages/FlashcardAutomation.py
@@ -2,6 +2,15 @@ import streamlit as st
 
 st.set_page_config(page_title="Flashcard Automation Tool", page_icon="🤖")
 
+with st.sidebar.expander("📊 Instagram Analyzer"):
+    st.page_link("pages/SeeWhoDoesntFollowYou.py", label="Home Page")
+
+with st.sidebar.expander("🤖 Flashcard Automation Tool"):
+    st.page_link("pages/FlashcardAutomation.py", label="Home Page")
+    
+with st.sidebar.expander("🎰 Casino"):
+    st.page_link("pages/Casino.py", label="Home Page")
+
 st.write(
     """
     This will be the page for the Flashcard Automation Tool! It's all under construction for now!
