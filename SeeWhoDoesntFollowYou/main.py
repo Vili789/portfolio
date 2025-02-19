f3491c85a9b0ab0abdaab7896d034d42b1115328
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-19 10:19:10 +0100

    Delete SeeWhoDoesntFollowYou directory

--- SeeWhoDoesntFollowYou/main.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Instagram Analyzer", page_icon="📊")
-
-st.write(
-    """
-    This will be the page for the instagram Analyzer! It's all under construction for now!
-    """)
