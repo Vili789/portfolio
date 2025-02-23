d329d4f172d487040fb1aab9d5b0505958e9b804
Vili789 <165499542+Vili789@users.noreply.github.com>
2025-02-23 14:06:37 +0100

    Update vintage_slots.py

--- pages/vintage_slots.py
+++ pages/vintage_slots.py
@@ -8,6 +8,8 @@ with st.sidebar.expander("Welcome!"):
     
 with st.sidebar.expander("📊 Instagram Analyzer"):
     st.page_link("pages/SeeWhoDoesntFollowYou.py", label="Home Page")
+    st.page_link("pages/doesnt_follow.py", label="See Who Doesn't Follow You Back")
+    st.page_link("pages/compare_changes.py", label="Compare Changes Over Time")
 
 with st.sidebar.expander("🤖 Flashcard Automation Tool"):
     st.page_link("pages/FlashcardAutomation.py", label="Home Page")
